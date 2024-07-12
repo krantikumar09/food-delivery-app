790b5c070b72b15def7d37751325d19f3faa1bbf
Krantikumar Mathapati <krantikumar3446@gmail.com>
2024-07-12 12:08:49 +0530

    created food related api's
    add food, list food and remove food

---
+++ backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect("mongodb://127.0.0.1:27017/thekitchn")
+    .then(() => console.log("Database Connected !"));
+};
