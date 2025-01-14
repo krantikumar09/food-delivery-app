2a0cac7db2a8c89f6358eb56771aff62b3f983df
Krantikumar Mathapati <krantikumar3446@gmail.com>
2025-01-14 19:14:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,23 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    output: {
+      assetFileNames: (assetInfo) => {
+        if (assetInfo.name === "favicon.ico") {
+          return "[name][extname]"; // Keep it in the root
+        }
+        return "assets/[name]-[hash][extname]";
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
